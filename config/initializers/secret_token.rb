867ffd05a598e1fa8a5bce1e7508b673f3a3a693
RickEaton <tech@sierraserviceproject.org>
2011-11-06 13:31:46 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spoic3::Application.config.secret_token = 'c871ea9eafb9701c680e9688109f3e50d3c0105334aa7b8687b069e199379e2d97004739b9ddc49ba5d128ce1f47c5f1fc68ba371840ea33b9f8d6a25b2feffd'
