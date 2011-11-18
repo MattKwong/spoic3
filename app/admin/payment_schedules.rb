ca078c28bd6f2f11ef1ee63ea2d6b8de4039649f
unknown <rickeaton@SSP-WS-Director.ssp.local>
2011-11-18 15:53:55 -0800

    Deploying successfully on Heroku!

--- app/admin/payment_schedules.rb
+++ app/admin/payment_schedules.rb
@@ -2,12 +2,17 @@ ActiveAdmin.register PaymentSchedule do
   menu :parent => "Configure Sites and Sessions"
 
    show do
-    attributes_table :name, :total_payment
+    attributes_table :name, :deposit, :second_payment, :final_payment, :total_payment
    end
 
   index do
     column :name
-    column "Total Fee", sortable => :total_payment do |ps|
+    column :deposit
+    column :second_payment
+#    column "Second Payment Date"
+    column :final_payment
+#    column "Final Payment Date"
+    column "Total Fee" do |ps|
       number_to_currency ps.total_payment
     end
   end
