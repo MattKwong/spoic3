d449bd7158d22f29336f943f5e04f62a63a3faf2
unknown <rickeaton@SSP-WS-Director.ssp.local>
2011-12-18 21:32:58 -0800

    Commit migrations

---
+++ app/models/budget_item.rb
@@ -0,0 +1,24 @@
+class BudgetItem < ActiveRecord::Base
+  belongs_to :budget_item_type, :foreign_key => :item_id
+  belongs_to :site
+  attr_accessible :amount, :item_id, :site_id
+
+  validates :item_id, :amount, :site_id, :presence => true
+  validates_numericality_of :amount, :greater_than_or_equal_to => 0, :decimal => true
+#  validate :site_and_item_unique
+  validate :site_valid
+  validate :item_valid
+
+  def site_valid
+    errors.add(:site_id, "Must enter a valid site.") unless Site.find(site_id)
+  end
+  def item_valid
+    errors.add(:item_id, "Must enter a valid item type.") unless BudgetItemType.find(item_id)
+  end
+
+  def site_and_item_unique
+    if BudgetItem.find(:conditions => ["item_id = ? AND site_id = ?", :item_id, :site_id]) then
+      errors.add(:site_id, "A budget already exists for this site and item.")
+    end
+  end
+end
---
+++ app/models/budget_item_type.rb
@@ -0,0 +1,9 @@
+class BudgetItemType < ActiveRecord::Base
+  has_many :budget_items
+
+  attr_accessible :description, :name, :seq_number
+
+  validates :name, :presence => true, :uniqueness => true
+  validates :description, :seq_number, :presence => true
+  validates_numericality_of :seq_number, :greater_than_or_equal_to => 0, :integer => true
+end
