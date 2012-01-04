06927b5e0102979d03f5730223d3b2d220992fad
unknown <rickeaton@SSP-WS-Director.ssp.local>
2012-01-04 14:29:51 -0800

    Admin user workaround part 2

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -10,29 +10,29 @@ class AdminUser < ActiveRecord::Base
   attr_accessible :email, :name, :user_role, :first_name, :last_name,
                   :liaison_id, :password, :password_confirmation, :remember_me
 
-#  validates :email, :uniqueness => true
-#  validates :first_name, :last_name, :user_role, :presence => true
-#  validates_inclusion_of :user_role, :in => UserRole.all.map {|i| i.role_name}
+  validates :email, :uniqueness => true
+  validates :first_name, :last_name, :user_role, :presence => true
+  validates_inclusion_of :user_role, :in => UserRole.all.map {|i| i.role_name}
 
   before_save :create_name
 
-#  scope :admin, where(:user_role == 'Admin')
-#  scope :liaison, where(:user_role == 'Liaison')
+  scope :admin, where(:user_role == 'Admin')
+  scope :liaison, where(:user_role == 'Liaison')
 
-#    def admin?
-#      self.user_role == "Admin"
-#    end
+    def admin?
+      self.user_role == "Admin"
+    end
 
- #   def liaison?
- #     self.user_role == "Liaison"
- #   end
+    def liaison?
+      self.user_role == "Liaison"
+    end
 
- #   def food_admin?
- #     self.user_role == "Food Admin"
- #   end
- #   def construction_admin?
- #     self.user_role == "Construction Admin"
- #   end
+    def food_admin?
+      self.user_role == "Food Admin"
+    end
+    def construction_admin?
+      self.user_role == "Construction Admin"
+    end
 
   private
 
