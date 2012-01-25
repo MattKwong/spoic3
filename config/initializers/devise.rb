a73c690e0d7728a62abd7df46928fe0a94d73b81
unknown <rickeaton@SSP-WS-Director.ssp.local>
2012-01-24 16:31:04 -0800

    Working on email configuration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "director@sierraserviceproject.org"
+  config.mailer_sender = "admin@sierraserviceproject.org"
 
   # Configure the class responsible to send e-mails.
 #   config.mailer = "Devise::Mailer"
