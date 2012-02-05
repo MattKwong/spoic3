ffbe960d1a12c024cab178aeeb6b037cd7db8bc5
unknown <rickeaton@SSP-WS-Director.ssp.local>
2012-02-05 10:19:41 -0800

    Made changes to date format in Dashboard to display local time.

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -5,7 +5,9 @@ ActiveAdmin::Dashboards.build do
   if proc{ @current_admin_user.admin? } #@current_admin_user.can? :read
      section "Recent System Activity" do
       table_for Activity.order("activity_date desc" ) do
-        column :activity_date, :format => :long
+        column :activity_date do |activity|
+          activity.activity_date.in_time_zone("Pacific Time (US & Canada)").strftime("%b %d, %Y %l:%M %p")
+        end
         column :user_name
         column :activity_type
         column :activity_details
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,7 @@ class Activity < ActiveRecord::Base
   validates  :activity_date, :activity_details, :activity_type, :user_id, :user_name, :user_role, :presence => true
   validates_numericality_of :user_id
 
+#  def activity_date
+#    self.activity_date = self.activity_date.in_time_zone("Pacific Time (US & Canada")
+#  end
 end
