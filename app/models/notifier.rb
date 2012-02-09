d639ad340c48ccd27e03495594e04c7884df2e0c
unknown <rickeaton@SSP-WS-Director.ssp.local>
2012-02-08 18:17:32 -0800

    Mail debugging

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,7 +1,7 @@
 class Notifier < ActionMailer::Base
 default :from => "admin@sierraserviceproject.com"
   def password_email(user)
-    @user = user
+    @resource = user
     mail( :to => user.email,
     :subject => 'SSP Password Reset Request')
   end
