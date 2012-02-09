368ab6b71f5647c32f618a3c10a33270ac98d8f7
unknown <rickeaton@SSP-WS-Director.ssp.local>
2012-02-08 18:05:19 -0800

    Mail debugging

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,20 +1,10 @@
 class Notifier < ActionMailer::Base
 default :from => "admin@sierraserviceproject.com"
   def password_email(user)
+    @user = user
     mail( :to => user.email,
-    :subject => 'SSP Password Reset Request',
-    :body => password_reset_body(user))
+    :subject => 'SSP Password Reset Request')
   end
 
-  def password_reset_body(user)
-    body = "Dear #{user.name},
 
-We have received a request to reset your MySSP password. To change your password, click on the link below.
-
- admin_users/password/edit?#{user.reset_password_token}
-
-Meghan Osborn, SSP Business Manager
-
-"
-  end
 end
