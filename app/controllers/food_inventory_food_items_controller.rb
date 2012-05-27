efee36f73f467251e25358968cfb2e58b358b8e9
rickeaton <rickeaton@sierraserviceproject.org>
2012-05-27 15:41:48 -0700

    Got food inventory item update to work

--- app/controllers/food_inventory_food_items_controller.rb
+++ app/controllers/food_inventory_food_items_controller.rb
@@ -7,22 +7,19 @@ class FoodInventoryFoodItemsController < ApplicationController
     @page_title = "Record Inventory Results"
     @food_inventory_food_item = FoodInventoryFoodItem.new
     @food_inventory_food_item.food_inventory_id= @food_inventory.id
-
   end
 
   def create
-    @food_inventory_food_item  = FoodInventoryFoodItem.new(params[:food_inventory_food_item ])
-    @food_inventory = @food_inventory_food_item.food_inventory
-#    logger.debug @project
+    @food_inventory_food_item  = FoodInventoryFoodItem.new(params[:food_inventory_food_item])
+    @food_inventory_food_item.food_inventory_id = params[:food_inventory_food_item]
+    @food_inventory = FoodInventory.find(params[:food_inventory_id])
     if @food_inventory_food_item.save
       flash[:success] = "Inventory results for #{@food_inventory_food_item.item.name} have been successfully recorded."
       redirect_to @food_inventory
     else
-      flash[:warning] = "Errors prevented this inventory record from being saved."
+      flash[:warning] = "Errors prevented from being saved: #{@food_inventory_food_item.errors.first[0].to_s} #{@food_inventory_food_item.errors.first[1].humanize}."
       @page_title = "#{@food_inventory.program.name} #{@food_inventory.date}: Record inventory Results"
-      @user_list = Hash[@project.program.program_users.food.map {|u| ["#{u.admin_user.name}", u.admin_user.id ]}]
-      refresh :new
+      render :new
     end
   end
-
 end
