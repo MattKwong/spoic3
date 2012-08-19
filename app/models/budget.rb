f29a11598a2b29601fab7a74623def3f58578db7
rickeaton <rickeaton@sierraserviceproject.org>
2012-08-19 14:40:40 -0700

    Removed budget class.

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,6 +1,6 @@
-class Budget < Base
-  has_many :budget_items
-  has_many :item_purchases
-  belongs_to :program
-
-end
+#class Budget
+#  has_many :budget_items
+#  has_many :item_purchases
+#  belongs_to :program
+#
+#end
